4676a63d7ccc073965ffe4401fc04769679d6715
skr9815 <skr9815@gmail.com>
2023-08-05 23:46:52 +0530

    v.1

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=" className='w-[24rem] min-w-[20rem] h-[3rem] glassCard p-5">
+    <a href="">
+      Git Link Source Code
+    </a>{" "}
+    | Developed by{" "}
+    <a target="_blank" href="">
+      Shubham</a>
+  </div>
+    )
+}
+
+export default Footer
