06daa83ef1932d9c25f52cbba5dfbce8380a52b0
skr9815 <skr9815@gmail.com>
2024-08-10 13:23:46 +0530

    v.02

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className=" className='w-[24rem] min-w-[20rem] h-[3rem] glassCard p-5">
-    <a href="">
+    <a href="https://github.com/skr9815/weather-app">
       Git Link Source Code
     </a>{" "}
     | Developed by{" "}
-    <a target="_blank" href="">
+    <a target="_blank" href="https://www.linkedin.com/in/skr9815/">
       Shubham</a>
   </div>
     )
